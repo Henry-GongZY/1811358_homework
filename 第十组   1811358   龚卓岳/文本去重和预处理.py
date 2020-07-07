e2d141b5e4360f4c7471a1abb2246e5f42b8894e
Henry-GongZY <1811358@mail.nankai.edu.cn>
2020-07-07 22:53:43 +0800

    .

--- 第十组   1811358   龚卓岳/文本去重和预处理.py
+++ 第十组   1811358   龚卓岳/文本去重和预处理.py
@@ -1,14 +1,15 @@
 import pymysql
 import pandas as pd
-conn = pymysql.connect(host="localhost", user="root", password="", database="user_comments")
-cursor = conn.cursor()
-sql='select content from user_comments.user_comments'
-cursor.execute(sql)
-curr=cursor.fetchall()   #从数据库拿出爬虫爬取到的数据,通过抽取特定列实现评论抽取
-comments=[]
-for i in curr:
-    comments.append(i[0].replace('\n','').replace('外形外观：','').replace('加热速度：','').replace('耗能情况：','').replace('其他特色：','').replace('洗浴时间：',''))
-    #去除换行符以及京东评论区保持评论结构统一用到的分割字段
-comments=pd.DataFrame(pd.Series(comments).unique())
-    #去重，生成csv文件并进行下一步处理
-comments.to_csv('comments.csv',encoding='utf-8',header=False)
+if __name__ == "__main__":
+    conn = pymysql.connect(host="localhost", user="root", password="", database="user_comments")
+    cursor = conn.cursor()
+    sql='select content from user_comments.user_comments'
+    cursor.execute(sql)
+    curr=cursor.fetchall()   #从数据库拿出爬虫爬取到的数据,通过抽取特定列实现评论抽取
+    comments=[]
+    for i in curr:
+        comments.append(i[0].replace('\n','').replace('外形外观：','').replace('加热速度：','').replace('耗能情况：','').replace('其他特色：','').replace('洗浴时间：',''))
+        #去除换行符以及京东评论区保持评论结构统一用到的分割字段
+    comments=pd.DataFrame(pd.Series(comments).unique())
+        #去重，生成csv文件并进行下一步处理
+    comments.to_csv('comments.csv',encoding='utf-8',header=False)
--- 第十组   1811358   龚卓岳/机械压缩.py
+++ 第十组   1811358   龚卓岳/机械压缩.py
@@ -1,4 +1,64 @@
 import pandas as pd
 
-comments=pd.read_csv('comments.csv',encoding='utf-8',header=None)
-comments=comments.set_index(0,drop=True)
+def depress(string):
+    filelist = [string]
+    filelist2 = []
+    for a_string in filelist:
+        temp1 = a_string.strip('\n')
+        temp2 = temp1.lstrip('\ufeff')
+        temp3 = temp2.strip('\r')
+        char_list = list(temp3)
+        list1 = []
+        list1.append(char_list[0])
+        list2 = ['']
+        del1 = []
+        i = 0
+        while (i < len(char_list)):
+            i = i + 1
+            if i == len(char_list):
+                if list1 == list2:
+                    m = len(list2)
+                    for x in range(i - m, i):
+                        del1.append(x)
+            else:
+                if char_list[i] == list1[0] and list2 == ['']:
+                    list2[0] = char_list[i]
+                elif char_list[i] != list1[0] and list2 == ['']:
+                    list1.append(char_list[i])
+                elif char_list[i] != list1[0] and list2 != ['']:
+                    if list1 == list2 and len(list2) >= 2:
+                        m = len(list2)
+                        for x in range(i - m, i):
+                            del1.append(x)
+                        list1 = ['']
+                        list2 = ['']
+                        list1[0] = char_list[i]
+                    else:
+                        list2.append(char_list[i])
+                elif char_list[i] == list1[0] and list2 != ['']:
+                    if list1 == list2:
+                        m = len(list2)
+                        for x in range(i - m, i):
+                            del1.append(x)
+                        list2 = ['']
+                        list2[0] = char_list[i]
+                    else:
+                        list1 = list2
+                        list2 = ['']
+                        list2[0] = char_list[i]
+        a = sorted(del1)
+        t = len(a) - 1
+        while (t >= 0):
+            del char_list[a[t]]
+            t = t - 1
+        str1 = ''.join(char_list)
+        str2 = str1.strip()
+        filelist2.append(str2)
+    return filelist2
+
+if __name__ == "__main__":
+    comments=pd.read_csv('comments.csv',encoding='utf-8',header=None)
+    comments=comments.set_index(0,drop=True)
+    for i in range(len(comments.index)):
+        comments.iloc[i][1]=(depress(comments.iloc[i][1])[0])
+    comments.to_csv('comments.csv', encoding='utf-8', header=False)
